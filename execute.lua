8654eba25375dcf32dc7512b997724c869a2c9cc
Sir otter <stevenson13bri@gmail.com>
2025-12-09 15:54:42 -0500

    Update execute.lua

--- execute.lua
+++ execute.lua
@@ -1,4 +1,1 @@
--- Bastion Client Execution Script
--- Copy and paste this into Roblox
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/yourusername/Bastion-Client/main/loadstring.lua"))()
